46d9d46e583dd11addce074f4399585ddf9a68a1
Roman Borisov <boriso-rom@yandex.ru>
2023-03-07 18:19:33 +0300

    added code to DZ_1

--- DZ_01/Project_3.java
+++ DZ_01/Project_3.java
@@ -4,11 +4,9 @@ import java.util.Scanner;
 
 public class Project_3 {
     public static void main(String[] args){
-        Scanner n = new Scanner(System.in);
-        System.out.print("Введите число: ");
-        int userInput = n.nextInt();
+        int n = 1000;
         int result = 0;
-        for (int i = 1; i <= userInput; i++) {
+        for (int i = 1; i <= n; i++) {
             result += 1;
             System.out.println(result);
         }
--- DZ_01/Project_5.java
+++ DZ_01/Project_5.java
@@ -1,3 +1,67 @@
+// *+Задано уравнение вида q + w = e, q, w, e >= 0. 
+// Некоторые цифры могут быть заменены знаком вопроса, например 2? + ?5 = 69. 
+// Требуется восстановить выражение до верного равенства. 
+// Предложить хотя бы одно решение или сообщить, что его нет
+
 public class Project_5 {
-    
+    public static void main(String[] args) {
+        System.out.println("Введите выражение: ");
+        String[] str = System.console().readLine().split(" ");
+        char[] charQ = str[0].toCharArray();
+        char[] charW = str[2].toCharArray();
+        char[] charE = str[4].toCharArray();
+        int temp = 0;
+        for (int i = 0; i < charE.length; i++) {
+            if (charQ[i] == '?') {
+                if (charW[i] == '?') {
+                    if (charE[i] == '?') {
+                        charQ[i] = charW[i] = charE[i] = '0';
+                    }
+                    else{
+                        charQ[i] = '0';
+                        charW[i] = charE[i];
+                    }
+                }
+                else{
+                    if (charE[i] == '?'){
+                        charE[i] = charW[i];
+                        charQ[i] = '0';
+                    }
+                    else{
+                        temp = (Character.getNumericValue(charE[i]) - Character.getNumericValue(charW[i]));
+                        if (temp < 0){
+                            temp += 10;
+                            charQ[i - 1] -= 1;
+                        }
+                        charQ[i] = (char)('0' + temp);
+                    }
+                }
+            }
+            else {
+                if (charW[i] == '?'){
+                    if (charE[i] == '?'){
+                        charW[i] = '0';
+                        charE[i] = charQ[i];
+                    }
+                    else {
+                        temp = (Character.getNumericValue(charE[i]) - Character.getNumericValue(charQ[i]));
+                        if (temp < 0){
+                            temp += 10;
+                            charW[i - 1] -= 1;
+                        }
+                        charW[i] = (char)('0' + temp);
+                    }
+                }
+                else {
+                    temp = (Character.getNumericValue(charQ[i]) + Character.getNumericValue(charW[i]));
+                    if (temp > 9) {
+                        temp -= 10;
+                        charE[i - 1] += 1;
+                    }
+                    charE[i] = (char)('0' + temp);
+                }
+            }
+        }
+        System.out.println(String.valueOf(charQ) + " + " + String.valueOf(charW) + " = " + String.valueOf(charE));
+    }
 }

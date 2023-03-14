9aac9e8781410603ba97ac4474acb3047db64cfc
Roman Borisov <boriso-rom@yandex.ru>
2023-03-14 20:58:48 +0300

    added code to tasks: 1, 2, 3

--- DZ_03/Task_2.java
+++ DZ_03/Task_2.java
@@ -2,6 +2,22 @@
 
 package DZ_03;
 
-public class Task_2 {
-    
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+class Task_2 {
+
+    public static void main(String[] args) {        
+        List <Integer> listInt = Arrays.asList(5,7,8,3,2,5);        
+        List <Integer> result = listInt.stream().reduce(0, (a, b) -> a + b)%2 == 0 ? 
+                filter(listInt, p->p%2!=0) : filter(listInt, p->p%2==0);
+        System.out.println("Массив: " + listInt);
+        System.out.println("Результат: " + result);              
+    }
+
+    private static List <Integer> filter(List <Integer> listInt, Predicate <Integer> predicate) {
+        return listInt.stream().filter(predicate).collect(Collectors.toList());
+    }
 }
--- DZ_03/Task_3.java
+++ DZ_03/Task_3.java
@@ -3,6 +3,47 @@
 
 package DZ_03;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Task_3 {
-    
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(1);
+        list.add(2);
+        list.add(8);
+        list.add(34);
+        list.add(-65);
+        list.add(6543);
+        list.add(-1);
+        list.add(0);
+
+        int min = list.get(0);
+        int max = list.get(0);
+        int average = list.get(0);
+
+        // Collections.sort(list);
+        // System.out.println(list.get(0));
+        // System.out.println(list.get(list.size() - 1));
+
+        for (Integer i: list) {
+        if(i < min) 
+            min = i;
+        if(i > max) 
+            max = i;
+        }
+
+        System.out.println("минимальное число: " + min);
+        System.out.println("максимальное число: " + max);
+        System.out.println("среднее арифметическое: " + average(args, list, average));
+
+    }
+    public static int average(String[] args, ArrayList<Integer> list, int average) {
+        int temp = 0;
+        for (int i = 0; i < list.size(); i++) {
+            temp = list.get(i) + temp;
+        }
+        average = temp / list.size();
+        return average;
+    }
 }
